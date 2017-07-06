61d3aac1764f61ff9b10e5527fae82397a8ff5de
Nicolas Galler <ngaller@gmail.com>
2017-07-06 11:24:09 -0400

    refactor to allow custom getRows method

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const Customer = require('./lib/customer'),
+  Currency = require('./lib/currency'),
   Supplier = require('./lib/supplier'),
   SalesTerritory = require('./lib/salesTerritory'),
+  ServiceBase = require('./lib/serviceBase'),
   Connection = require('./lib/connection')
 
 const identity = x => x
@@ -10,8 +12,10 @@ function Epicor({serverUrl, username, password, company, strictSSL}, serviceAdap
 
   this.getConnection = () => connection
   this.Customer = serviceAdapter(new Customer(connection))
+  this.Currency = serviceAdapter(new Currency(connection))
   this.Supplier = serviceAdapter(new Supplier(connection))
   this.SalesTerritory = serviceAdapter(new SalesTerritory(connection))
+  this.Terms = serviceAdapter(new ServiceBase(connection, 'Erp.BO.TermsSvc', 'Terms', 'TermsCode'))
 }
 
 module.exports = Epicor
