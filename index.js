f62041fc8f4794d5552602b7a7947512ca654399
Nicolas Galler <ngaller@gmail.com>
2017-07-17 12:31:41 -0400

    Add resource group

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Customer = require('./lib/customer'),
   Currency = require('./lib/currency'),
   Employee = require('./lib/employee'),
+  ResourceGroup = require('./lib/resourceGroup'),
   Supplier = require('./lib/supplier'),
   SalesTerritory = require('./lib/salesTerritory'),
   ServiceBase = require('./lib/serviceBase'),
@@ -15,7 +16,7 @@ function Epicor({serverUrl, username, password, company, strictSSL}) {
   this.Currency = new Currency(connection)
   this.Customer = new Customer(connection)
   this.PerCon = new ServiceBase(connection, 'Erp.BO.PerConSvc', 'PerCon', 'PerConID')
-  this.ResourceGroup = new ServiceBase(connection, 'Erp.BO.ResourceGroupSvc', 'ResourceGroup', 'ResourceGrpID')
+  this.ResourceGroup = new ResourceGroup(connection)
   this.SalesTerritory = new SalesTerritory(connection)
   this.ShipVia = new ServiceBase(connection, 'Erp.BO.ShipViaSvc', 'ShipVia', 'ShipViaCode')
   this.Supplier = new Supplier(connection)
