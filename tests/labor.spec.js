cd508ef2a3164ab21b55b409e8092e9a31902ca1
Nicolas Galler <ngaller@gmail.com>
2017-11-09 14:01:39 -0500

    Labor service

---
+++ tests/labor.spec.js
@@ -0,0 +1,30 @@
+const Connection = require('../lib/connection.js'),
+  Labor = require('../lib/labor.js')
+
+describe('Labor Service', () => {
+  let connection, laborSvc
+
+  beforeEach(() => {
+    connection = new Connection({
+      serverUrl: 'https://nowhere.com'
+    })
+    laborSvc = new Labor(connection)
+  })
+
+  describe('find', () => {
+    it('calls GetRows', (done) => {
+      const r = sinon.stub()
+        .returns(new Promise(function() {}))
+      laborSvc.makeRequest = r
+      const result = laborSvc.find('testing')
+      expect(result).to.have.property('on')
+      result.on('data', () => {
+        throw new Error('there should be no data')
+      })
+      setImmediate(() => {
+        expect(r).to.have.been.calledWith('GetRows', sinon.match({whereClauseLaborHed: 'testing', pageSize: 25, absolutePage: 0}))
+        done()
+      })
+    })
+  })
+})
