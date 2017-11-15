3e5265af159432ad5147ce8dcacfdc8e6d75cb33
Nicolas Galler <ngaller@gmail.com>
2017-11-15 13:32:58 -0500

    code for adding labor entries

--- tests/setup.js
+++ tests/setup.js
@@ -1,9 +1,11 @@
 const sinon = require('sinon'),
   chai = require('chai'),
-  sinonChai = require('sinon-chai')
+  sinonChai = require('sinon-chai'),
+  dotenv = require('dotenv')
 
 chai.use(sinonChai)
 chai.should()
 global.should = chai.should
 global.expect = chai.expect
 global.sinon = sinon
+dotenv.configure()
