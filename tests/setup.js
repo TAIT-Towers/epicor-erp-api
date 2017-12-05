fdabe3a545baa0b76a63fa43215ec50f0dfc7e28
Nicolas Galler <ngaller@gmail.com>
2017-12-05 08:55:55 -0500

    Validate submission under 24 hours

--- tests/setup.js
+++ tests/setup.js
@@ -1,9 +1,11 @@
 const sinon = require('sinon'),
   chai = require('chai'),
   sinonChai = require('sinon-chai'),
+  chaiAsPromised = require("chai-as-promised")
   dotenv = require('dotenv')
 
 chai.use(sinonChai)
+chai.use(chaiAsPromised)
 chai.should()
 global.should = chai.should
 global.expect = chai.expect
